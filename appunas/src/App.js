96ec525a3e03e31317cd0d5e54e27f1b50d57c5c
Nico Campisi <nicocampisi98@gmail.com>
2022-06-17 15:50:27 -0300

    card

--- appunas/src/App.js
+++ appunas/src/App.js
@@ -4,6 +4,8 @@ import NavBar from './componentes/NavBar/NavBar';
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './componentes/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   const [page, setPage] = useState('list')
   return (
--- appunas/src/componentes/Item/Item.js
+++ appunas/src/componentes/Item/Item.js
@@ -1,16 +1,45 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import {Card, Button} from "react-bootstrap";
 const Item = ({id, name, img, price}) => {
-  return (
-    <article>
-      <li>
-          {name}
-          <img src={img} alt={name}/>
-          {price}
-      </li>
-      <footer>
+  const cardInfo = [
+    {image: "", title: "Lebron", text:"THE GOAT"},
+    {image: "", title: "Harden", text:"HE GOOD"},
+    {image: "", title: "Curry", text:"HE GOOD"},
+    {image: "", title: "Jordan", text:"HE IS VERY CLOSE TO GOAT"}
+  ];
+
+  const renderCard = (card, index) => {
+    return (
+    <Card style={{ width: '18rem' }} key={index}>
+    <Card.Img variant="top" src="holder.js/100px180" src={img} />
+    <Card.Body>
+      <Card.Title>{name}</Card.Title>
+      <Card.Text>
+        {price}
+      </Card.Text>
+      <Button>
         <Link to={`/detail/${id}`}> Ver detalle </Link>
-      </footer>
-    </article>
+      </Button>
+    </Card.Body>
+    
+  </Card>
+
+    )
+  }
+  return (
+    <div>
+      {cardInfo.map(renderCard)}
+    </div>
+    //  <article>
+    //    <li>
+    //        {name}
+    //        <img src={img} alt={name}/>
+    //        {price}
+    //   </li>
+    //   <footer>
+    //    <Link to={`/detail/${id}`}> Ver detalle </Link>
+    //    </footer>
+    //  </article>
   )
 }
 
